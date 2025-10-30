2768ca40fe806b7c2b385706b50e7ef2961ebe4c
Maurício Soares <mauricio.soares2208@gmail.com>
2025-10-30 11:39:30 -0300

    commit da base

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Suporte</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<center>
+
+    <h1>Sistema de Suporte</h1>
+    <p style="margin-top: -22.5px;">Feito por Maurício e Cauan</p>
+
+    <div>
+
+    <h3></h3>
+    <input placeholder="" type="text" name="" id="equip">
+    <h3>Local do equipamento</h3>
+    <input placeholder="" type="text" name="" id="local">
+    <h3>Data e hora da abertura do chamado</h3>
+    <input type="date" name="" id="date_equip">
+    <input type="time" name="" id="hora_equip">
+    <h3>Responsável pela abertura/h3>
+    <input placeholder="" type="text" name="" id="nome">
+<br> <br>
+    <button value="">Concluir</button>
+
+    </div>
+
+    </center>
+</body>
+</html>
